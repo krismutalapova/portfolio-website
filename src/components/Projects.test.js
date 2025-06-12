fc000af49bc5ece35aef58fe94abb249bbf32f6d
krismutalapova <kris.mutalapova@pm.me>
2025-06-12 16:17:07 +0200

    test: split and expand component tests

---
+++ src/components/Projects.test.js
@@ -0,0 +1,22 @@
+import { render, screen, waitFor, queryByText } from '@testing-library/react';
+import Projects from './Projects';
+
+beforeEach(() => {
+    window.matchMedia = jest.fn().mockImplementation(query => ({
+        matches: false,
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        addListener: jest.fn(),
+        removeListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+    }));
+});
+
+describe('Projects', () => {
+    it('renders section with project title', async () => {
+        render(<Projects />);
+        // Wait for the spinner to disappear
+        await waitFor(() => expect(screen.queryByRole('status')).not.toBeInTheDocument(), { timeout: 2000 });
+        expect(await screen.findByText(/todo app/i)).toBeInTheDocument();
+    });
+});
